b5309bb5db47408dba7485756729888f07954a36
Daniel Boterhoven <hello@danielboterhoven.tech>
2016-03-23 16:51:12 +0800

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function main() {
+    
+    // Exercise #1
+    // Change the body tag's style so it has a font-family of "Arial, sans-serif".
+    
+    
+    // Exercise #2
+    // Replace each of the spans (firstName, favouriteFood, hometown) with your own information.  
+    
+    
+    // Exercise #3
+    // Iterate through each li and change the class to "list-item". Add a style tag that sets a rule for "list-item" to make the color red.  
+    
+    
+    // Exercise #4
+    // Create a new img element and set its src attribute to a picture of you. Append that element to the page. 
+    
+    
+}
+
+function buttonPress() {
+    
+    // Exercise #5
+    // Update the button lable to display 'Pressed at 16:49:00 GMT+0800 (W. Australia Standard Time)' when it's clicked. You'll need to first lookup how to determine the time in JavaScript before you can set the button label!
+    
+    
+}
